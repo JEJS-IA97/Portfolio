dbcdd82fee64e57ef7553312e21b05e722ef0991
José Eduardo Jiménez <136139000+JEJS-IA97@users.noreply.github.com>
2025-01-06 23:58:41 -0400

    Update

--- src/components/Front.jsx
+++ src/components/Front.jsx
@@ -11,14 +11,14 @@ const Front = () => {
         />
   
         <div className="z-10 items-center flex flex-grow w-full lap:p-[80px] mob:p-[40px] tab:p-[60px] h-full mt-[100px] ">
-          <div className="flex flex-col top-0 absolute z-20 mob:mt-[95px] tab:mt-[160px]">
+          <div className="flex flex-col top-0 absolute z-20 mob:mt-[95px] tab:mt-[160px] lap:mt-[215px]">
             <h1 className="lap:text-[48px] mob:text-[24px] tab:text-[34px] font-bold text-white mb-2 mob:w-[160px] tab:w-[385px] lap:w-[700px] leading-7">
               <span className="text-GreenB">Hello</span>, I'm José Jiménez
             </h1>
             <h2 className="lap:text-[20px] mob:text-[14px] tab:text-[17px]  font-medium text-white z-20">
               Computer Engineer
             </h2>
-            <h2 className="z-20 lap:text-[14px] mob:text-[12px] tab:text-[13px] tab:w-[385px]   font-normal text-white mob:w-[160px]">
+            <h2 className="z-20 lap:text-[14px] mob:text-[12px] tab:text-[13px] tab:w-[385px] lap:w-[700px] font-normal text-white mob:w-[160px]">
             Frontend Developer, Quality Assurance & Graphic/UI Designer.
             </h2>
           </div>
